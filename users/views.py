94f307ff7c2a3e9ffc72b0e922923221fc8352af
Yesenin <bellfortblood@gmail.com>
2026-07-23 17:14:37 +0300

    Личный кабинет работает

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            users = form.save()
+            login(request, users)
+            return redirect('dashboard')
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
