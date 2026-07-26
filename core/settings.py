6d79260ad3ca7cfbe8062a65139acbf1a2e18e2b
Yesenin <bellfortblood@gmail.com>
2026-07-26 17:35:37 +0300

    Добавлен Celety

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json']
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_TIMEZONE = 'UTC'
