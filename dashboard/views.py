3bc79d042f03306f8aca8d8d176b116bdef6da43
Yesenin <bellfortblood@gmail.com>
2026-07-26 20:07:05 +0300

    Список ботов готов

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        bots = self.request.user.bots.all()
+        context['bots'] = bots
+        return context
